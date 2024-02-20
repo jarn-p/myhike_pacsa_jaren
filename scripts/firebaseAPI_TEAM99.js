ba9c97beb5779ff24525d454dacbf64ba4c97f96
Jaren Pacsa <jarenpacsa@gmail.com>
2024-02-20 13:27:29 -0800

    modified html files

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyA_qt-aDDSTGitBy-Vk20kfwgYK4-F5_gk",
+    authDomain: "comp1800-202410-demo07.firebaseapp.com",
+    projectId: "comp1800-202410-demo07",
+    storageBucket: "comp1800-202410-demo07.appspot.com",
+    messagingSenderId: "554903954400",
+    appId: "1:554903954400:web:f5d2a0106812a61fd30185"
 };
 
 //--------------------------------------------
